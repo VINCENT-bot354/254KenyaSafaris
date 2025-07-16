1597eae39da8baead190ba42ba5de23d5eecb52e
VINCENT-bot354 <vincentnguthiru123@gmail.com>
2025-07-16 20:35:34 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 from functools import wraps
 from app import app, mail
 from utils import load_data, save_data, allowed_file, get_file_url
+from reviews import get_top_reviews
 
 # Admin password
 ADMIN_PASSWORD = "254Safaris@2025"
@@ -24,7 +25,8 @@ def index():
     content = load_data('content.json')
     packages = load_data('packages.json')
     gallery = load_data('gallery.json')
-    return render_template('index.html', content=content, packages=packages[:3], gallery=gallery[:6])
+    reviews = get_top_reviews(limit=5, min_stars=4)
+    return render_template('index.html', content=content, packages=packages[:3], gallery=gallery[:6], reviews=reviews)
 
 @app.route('/destinations')
 def destinations():
