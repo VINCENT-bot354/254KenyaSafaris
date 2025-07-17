30d9b7ddb9b7692cf675b991107a6620dae8707b
VINCENT-bot354 <vincentnguthiru123@gmail.com>
2025-07-17 21:02:11 +0300

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -1,5 +1,15 @@
-from flask import Blueprint, render_template, request, redirect, url_for import os import psycopg2 from dotenv import load_dotenv
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for
+)
+import os
+import psycopg2
+from dotenv import load_dotenv
 
+load_dotenv()
 Load environment variables from .env file
 
 load_dotenv()
